b7573a372d41546affe6c4f163d76e901dd7faf5
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-08-26 10:45:26 +0100

    feat: Add of function export in **LopNeuralNetwork**

--- src/LopNeuralNetwork.jl
+++ src/LopNeuralNetwork.jl
@@ -1,5 +1,10 @@
 module LopNeuralNetwork
+using Metrics
+using Plots: plot
+using ProgressBars
+using Random: seed!, TaskLocalRNG
+export NeuralNetwork, forward_propagation, back_propagation, predict, score, fit!
+include("Network/Network.jl")
 
-# Write your package code here.
 
 end
